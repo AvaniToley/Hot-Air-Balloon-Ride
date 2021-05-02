4a76317c895b8597a337b838fa933fc656504636
AvaniToley <79001295+AvaniToley@users.noreply.github.com>
2021-05-02 18:35:07 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,56 @@
+var balloon,balloonImage1,balloonImage2;
+// create database and position variable here
+var database;
+var position;
+
+function preload(){
+   bg =loadImage("cityImage.png");
+   balloon=loadAnimation("hotairballoon1.png");
+   balloonImage2=loadAnimation("hotairballoon1.png","hotairballoon1.png",
+   "hotairballoon1.png","hotairballoon2.png","hotairballoon2.png",
+   "hotairballoon2.png","hotairballoon3.png","hotairballoon3.png","hotairballoon3.png");
+
+   var balloon = database.ref('ball/position');
+   balloon.on("value", readPosition, showError);
+  }
+
+//Function to set initial environment
+function setup() {
+  database=firebase.database();
+  console.log(database);
+  createCanvas(1500,700);
+
+  balloon=createSprite(250,450,150,150);
+  balloon.addAnimation("hotAirBalloon",balloonImage1);
+  balloon.scale=0.5;
+
+  textSize(20); 
+}
+
+// function to display UI
+function draw() {
+  background(bg);
+
+  if(keyDown(LEFT_ARROW)){
+    writePosition(-1,0);
+  }
+  else if(keyDown(RIGHT_ARROW)){
+    writePosition(1,0);
+  }
+  else if(keyDown(UP_ARROW)){
+    writePosition(0,-1);
+  }
+  else if(keyDown(DOWN_ARROW)){
+    writePosition(0,+1);
+  }
+  
+  drawSprites();
+  fill(0);
+  stroke("white");
+  textSize(25);
+  text("Use arrow keys to move Hot Air Balloon!",40,40);
+}
+
+
+  
+
